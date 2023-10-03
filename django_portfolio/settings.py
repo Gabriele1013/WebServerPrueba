d7e0af48f2521bfdec9e1896b9e7fbaa700e44a3
Gabriele1013 <137875233+Gabriele1013@users.noreply.github.com>
2023-10-03 01:51:21 -0500

    Update settings.py

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -82,10 +82,6 @@ WSGI_APPLICATION = 'django_portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default = 'postgresql://postgres:postgres@localhost/postgres',
-        conn_max_age=600
-    )
     'default': {
         'ENGINE':'django.db.backends.postgresql_psycopg2',
         'NAME':'gabriele',
